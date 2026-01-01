21d782f79539fc14685fef3738575ecd25f82696
unknown <leminhtu74@gmail.com>
2026-01-01 21:29:06 +0700

    feat: loop video

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <opencv2/opencv.hpp>
+#include <chrono>
+#include <thread>
 #include <iostream>
 
 int main() {
@@ -9,18 +11,61 @@ int main() {
         return -1;
     }
 
-    cv::Mat frame;
-    cap >> frame;
+    double fps = cap.get(cv::CAP_PROP_FPS);
+    std::cout << "FPS: " << fps << " ms\n";
 
-    if (frame.empty()) {
-        std::cerr << "Error: first frame is empty\n";
-        return -1;
+    cv::Mat frame;
+    int frameCount = 0;
+
+    
+    cv::namedWindow("Video", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Video", 1280, 720);
+    
+    auto lastTime = std::chrono::steady_clock::now();
+    int targetFrameMs = static_cast<int>(1000.0  / fps);
+    auto lastReportTime = std::chrono::steady_clock::now();
+    auto lastFrameTime = lastReportTime;
+
+    while (true) {
+        cap >> frame;
+
+        if (frame.empty()) {
+            cap.set(cv::CAP_PROP_POS_FRAMES, 0);
+            continue;
+        }
+        
+        frameCount++;
+        cv::imshow("Video", frame);
+
+        auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(
+            now - lastTime
+        ).count();
+
+        if (elapsed >= 5) {
+            std::cout << frameCount << std::endl;
+            lastTime = now;
+        }
+
+        cv::waitKey(1);
+        
+        auto frameEnd = std::chrono::steady_clock::now();
+        auto frameElapsedMs = std::chrono::duration_cast<std::chrono::milliseconds>(
+            frameEnd - lastFrameTime
+        ).count();
+
+        int sleepMs = targetFrameMs - static_cast<int>(frameElapsedMs);
+        if (sleepMs > 0) {
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(sleepMs)
+            );
+        }
+
+        lastFrameTime = std::chrono::steady_clock::now();
     }
 
-    cv::imwrite("first_frame.jpg", frame);
-
-    cv::imshow("Image", frame);
-    cv::waitKey(0);
+    cap.release();
+    cv::destroyAllWindows();
 
     return 0;
 }
